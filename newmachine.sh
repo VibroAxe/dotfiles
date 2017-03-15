76b1fa30be0490b32927683de4728a7e00f94fd6
VibroAxe <vibs@macrolevel.co.uk>
2017-03-15 09:09:00 +0000

    Improved ssh key handling
    Now wipes known_hosts list and replaces authorized_keys with id_rsa.pub, further keys may then be added manually on a server by server basis

--- newmachine.sh
+++ newmachine.sh
@@ -3,7 +3,8 @@
 # attempt to grab ssh keys from core.tnnt
 echo "Attempting to grab ssh keys from tnnt"
 scp -r vibs@core.tnnt.co.uk:.ssh/ ~/
-rm ~/.ssh/authorized_keys
+cp ~/.ssh/id_rsa.pub ~/.ssh/authorized_keys
+rm ~/.ssh/known_hosts
 
 # Assume debian/ubuntu
 sudo apt-get install sudo tmux git vim
@@ -20,4 +21,3 @@ git clone git@github.com:VibroAxe/dotfiles.git ~/.dotfiles
 
 #Clone scripts and install
 git clone git@github.com:VibroAxe/scripts.git ~/scripts
-
