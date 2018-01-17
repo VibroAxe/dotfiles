b3ca0db4eba11cc1298574d48464bc1636683494
VibroAxe <vibs@macrolevel.co.uk>
2018-01-17 09:26:25 +0000

    Added ssh_agent commands

--- bash/bash-rc.d/ssh.sh
+++ bash/bash-rc.d/ssh.sh
@@ -1,14 +1,38 @@
 #!/bin/bash
+
+
+# ssh-agent configuration
+if [ -z ${SSH_AUTH_SOCK+x} ]; then
+	#SSH_AUTH_SOCK is unset
+	if [ -z "$(pgrep ssh-agent)" ]; then
+		#rm -rf /tmp/ssh-*
+		eval $(ssh-agent -s) > /dev/null
+	else
+		export SSH_AGENT_PID=$(pgrep ssh-agent)
+		export SSH_AUTH_SOCK=$(find /tmp/ssh-* -name agent.$SSH_AGENT_PID)
+	fi
+else
+	#Using existing SSH_AUTH_SOCK (probably from ssh)
+	:
+fi
+
 ssh() {
 
 	#if [ "$(ps -p $(ps -p $$ -o ppid=) -o comm=)" = "tmux" ]; then
-if [[ -v TMUX ]]; then
-	#tmux rename-window "$(echo $* | cut -d . -f 1)"
-	tmux rename-window "ssh: $(echo $* | rev | cut -d ' ' -f1 | rev | cut -d . -f 1)"
-	command ssh "$@"
-	tmux set-window-option automatic-rename "on" 1>/dev/null
-else
-	command ssh "$@"
-fi
+	if [[ -v TMUX ]]; then
+		#tmux rename-window "$(echo $* | cut -d . -f 1)"
+		tmux rename-window "ssh: $(echo $* | rev | cut -d ' ' -f1 | rev | cut -d . -f 1)"
+		ssh_connect "$@"
+		tmux set-window-option automatic-rename "on" 1>/dev/null
+	else
+		ssh_connect "$@"
+	fi
 
+}
+
+ssh_connect() {
+	if [ "$(ssh-add -l)" == "The agent has no identities." ]; then
+   	 ssh-add
+	fi
+	command ssh -A "$@"
 }
--- bash/bash-rc.d/tmuxr.sh
+++ bash/bash-rc.d/tmuxr.sh
@@ -5,11 +5,11 @@ tmuxr() {
 if [[ -v TMUX ]]; then
 	#tmux rename-window "$(echo $* | cut -d . -f 1)"
 	tmux rename-window "tmux: $(echo $@ | rev | cut -d ' ' -f1 | rev | cut -d . -f 1)"
-	 { ( sleep 1 && tmux source-file ~/.config/tmux/tmux-enterremote.conf) & disown; } 2>/dev/null && command ssh "$@" -t -C "tmux a || tmux"
+	 { ( sleep 1 && tmux source-file ~/.config/tmux/tmux-enterremote.conf) & disown; } 2>/dev/null && ssh_connect "$@" -t -C "tmux a || tmux"
 	tmux source-file ~/.config/tmux/tmux-leaveremote-commands.conf
 	tmux set-window-option automatic-rename "on" 1>/dev/null
 else
-	command ssh "$@" -t -C "tmux a || tmux"
+	ssh_connect "$@" -t -C "tmux a || tmux"
 fi
 
 }
