d36432170bc937d482b2712179044757c3e4b54b
VibroAxe <vibs@macrolevel.co.uk>
2021-06-02 12:49:15 +0100

    QOL Changes
    Moved to using bashrc.d instead of bash-rc to avoid the stupid -
    Upgraded tmuxr to allow spawning a tmux instance on local host
    	tmuxr
    	tmuxr bastion
    Minor vim changes to silent the utf8 failure on systems which don't
    support (this should be pretty rare now)
    Added svi alias uses (sudoedit / sudo vi) to get a proper sudo vi whilst
    retaining local privs (note: uses temporary files so don't try :w file

---
+++ bash/bashrc.d/01-tmuxr.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#alias tmux-next to tmux if present
+which tmux-next 2>/dev/null >/dev/null && alias tmux='tmux-next -2' || alias tmux="tmux -2"
+
+
+tmuxr() {
+	if [[ "x$@" = "x" ]]; then
+		#If no host, reconnect to the existing session or start a new one if none exists
+		tmux a || tmux
+	else
+		#if [ "$(ps -p $(ps -p $$ -o ppid=) -o comm=)" = "tmux" ]; then
+		if [[ -v TMUX ]]; then
+			#tmux rename-window "$(echo $* | cut -d . -f 1)"
+			tmux rename-window "tmux: $(echo $@ | rev | cut -d ' ' -f1 | rev | cut -d . -f 1)"
+			 { ( sleep 1 && tmux source-file ~/.config/tmux/tmux-enterremote.conf) & disown; } 2>/dev/null && ssh_connect "$@" -t -C "tmux a || tmux"
+			tmux source-file ~/.config/tmux/tmux-leaveremote-commands.conf
+			tmux set-window-option automatic-rename "on" 1>/dev/null
+		else
+			ssh_connect "$@" -t -C "tmux a || tmux"
+		fi
+	fi
+
+}
+
+ssh-tmux() {
+	tmuxr $@
+}
+
+tmux-ssh() {
+	tmuxr $@
+}
+
+
