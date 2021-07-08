bf693c2abf1027ac43311c008ce2c3b4baf63ce1
VibroAxe <vibs@macrolevel.co.uk>
2021-07-08 11:12:38 +0100

    Fixed tmuxr to allow agent parsing
    Fixed tmuxr to allow agent parsing by spawning the tmux process in a
    bash shell
    
    Fixed newmachine.sh not git cloning the right repo

--- newmachine.sh
+++ newmachine.sh
@@ -3,7 +3,7 @@
 sudo apt-get install sudo git
 
 #Clone dotfiles and install
-git clone git@gitlab.com:VibroAxe/dotfiles.git ~/.config
+git clone git@github.com:VibroAxe/dotfiles.git ~/.config
 
 ~/.config/install.sh
 
