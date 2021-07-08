c498dce61352c76a45b7cd43bc69e75198a8724f
VibroAxe <vibs@macrolevel.co.uk>
2021-07-08 11:20:40 +0100

    Updated newmachine to use hybrid https/ssh gitpull

--- newmachine.sh
+++ newmachine.sh
@@ -1,9 +1,16 @@
+# Run this script is run to init a new machine
+# It is not run by VS/Github Codespaces
 
-# Assume debian/ubuntu and install tmux-next
+# Assume debian/ubuntu and install sudo/git
 sudo apt-get install sudo git
 
-#Clone dotfiles and install
-git clone git@github.com:VibroAxe/dotfiles.git ~/.config
+# Clone dotfiles
+git clone https://github.com/VibroAxe/dotfiles.git ~/.config
 
+# Update push url to use ssh
+cd ~/.config
+git remote set-url origin --push git@github.com:VibroAxe/dotfiles.git
+
+# And install
 ~/.config/install.sh
 
